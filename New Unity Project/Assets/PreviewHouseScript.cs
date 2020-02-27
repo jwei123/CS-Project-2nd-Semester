4bcbaded0357b9be944e05f0e3c3989990d57609
KleinBottle1 <61476702+KleinBottle1@users.noreply.github.com>
2020-02-27 13:52:06 -0800

    worked on house building code and stuff

---
+++ New Unity Project/Assets/PreviewHouseScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewHouseScript : MonoBehaviour
+{
+    public Camera gameCamera;
+    public GameObject TransparentHouse;
+    public GameObject PlacedHouse;
+    private bool housePlacing;
+    void Start() {
+        housePlacing = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(/******left mouse********/)) {
+            Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo)) {
+
+            }
+        }
+    }
+}

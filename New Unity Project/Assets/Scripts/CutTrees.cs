694cbddb7d5ee425c8be816f44dd228dced8ba54
CelestialSloth <esdemetrion@gmail.com>
2020-03-09 13:36:49 -0700

    Making trees highlight/die

--- New Unity Project/Assets/Scripts/CutTrees.cs
+++ New Unity Project/Assets/Scripts/CutTrees.cs
@@ -6,6 +6,7 @@ public class CutTrees : MonoBehaviour
 {
     public float woodAmount; //Fix later with connection to resource bar. Right now we are assuming one tree = one piece of wood
     public bool cutWoodNow = false;
+    public Material mat1;
 
     private void Update()
     {
@@ -13,23 +14,49 @@ public class CutTrees : MonoBehaviour
         {
             cutDownTrees(new Vector3(Random.Range(0, 500), 0, Random.Range(0, 500)), 40);
             cutWoodNow = false;
-            Debug.Log("I tried to cut trees");
         }
        
     }
-
-    void cutDownTrees(Vector3 location, float radius)
+    
+    //find trees within some radius. Use them for whatever.
+    public List<GameObject> findTreesInRadius(Vector3 location, float radius)
     {
+        List<GameObject> myTrees = new List<GameObject>();
         //go through each tree in this area. If it's close to the place of killing trees, destroy the tree.
-        for(int i = transform.childCount - 1; i >= 0; i --)
+        for (int i = transform.childCount - 1; i >= 0; i--)
         {
             //these should be global positions!
-            if(Vector3.Distance(transform.GetChild(i).position, location) < radius)
+            if (Vector3.Distance(transform.GetChild(i).position, location) < radius)
             {
-                Destroy(transform.GetChild(i).gameObject);
-                woodAmount++;
+                myTrees.Add(transform.GetChild(i).gameObject);
             }
-            
+
+        }
+
+        return myTrees;
+    }
+
+    //destroy the trees
+    public void cutDownTrees(Vector3 location, float radius)
+    {
+        List<GameObject> treesToChop = findTreesInRadius(location, radius);
+        for(int i = treesToChop.Count - 1; i >= 0; i--)
+        {
+            Destroy(treesToChop[i]);
+        }
+    }
+
+    public void highlightTrees(Vector3 location, float radius)
+    {
+        List<GameObject> treesToHighlight = findTreesInRadius(location, radius);
+        for(int i = transform.childCount - 1; i >= 0; i --)
+        {
+            //make it green
+        }
+
+        for(int i = 0; i < treesToHighlight.Count; i ++)
+        {
+
         }
     }
     
---
+++ New Unity Project/Assets/Scripts/TreeChopClickUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeChopClickUI : MonoBehaviour
+{
+    /*Call this script when we are in "Tree Chopping" mode.*/
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //create highlight
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //when we are in tree chopping mode, move the highlight with the mouse.
+        //thing that can send ray from camera through mouse position
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        //if it hit something, set the destination to the ray hit area
+        if (Physics.Raycast(ray, out hit))
+        {
+            transform.GetComponent<CutTrees>().cutDownTrees(hit.point, 20);
+        }
+        //when mouse is clicked, "chop" trees using CutTrees script
+        //hide the highlight otherwise
+    }
+}

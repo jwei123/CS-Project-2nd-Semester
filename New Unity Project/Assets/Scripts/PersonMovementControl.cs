27e624853a128040217b5cd5ece6da2a64bea7b2
Jack Weinberg <jweinberg0992@hotmail.com>
2020-03-09 12:28:52 -0700

    checking main

--- New Unity Project/Assets/Scripts/PersonMovementControl.cs
+++ New Unity Project/Assets/Scripts/PersonMovementControl.cs
@@ -17,7 +17,7 @@ public class PersonMovementControl : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(running == false)
         {
